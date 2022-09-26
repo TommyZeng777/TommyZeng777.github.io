6a5a11ec8cb10bd031487fdc1ae06dd3bc300e2f
TommyZeng777 <tommyzeng1031@gmail.com>
2022-09-26 10:18:08 +0800

    删除fonts上传，源文件瘦身

--- docs/.vuepress/components/IndexBigImg.vue
+++ docs/.vuepress/components/IndexBigImg.vue
@@ -476,7 +476,8 @@ function nextAllTipElement(elem) {
 .vdoing-index-class .home-wrapper .banner {
   margin-top: 0 !important;
   height: 100vh;
-  background-attachment: fixed !important;
+  /* background-attachment: fixed !important; */
+  
 }
 /* 图片中间的签名和标题位置 */
 .banner-conent {
