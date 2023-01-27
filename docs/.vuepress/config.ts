bcc2bf0607475d4d09f3ff2fab060db1f2165515
TommyZeng777 <tommyzeng1031@gmail.com>
2023-01-28 06:51:01 +0800

    更新了部分内容，加快LOGO加载速度

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -89,8 +89,8 @@
  
      // 侧边栏配置
      sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
-     logo: 'https://gcore.jsdelivr.net/gh/TommyZeng777/picgo/img/202301271349720.svg', // 导航栏logo
-     repo: 'TommyZeng777/TommyZeng777.github.io', // 导航栏右侧生成Github链接
+     logo: 'img/无底logo.svg', // 导航栏logo
+    //  repo: 'TommyZeng777/TommyZeng777.github.io', // 导航栏右侧生成Github链接
      searchMaxSuggestions: 10, // 搜索结果显示最大数
      lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
      docsDir: 'docs', // 编辑的文件夹
