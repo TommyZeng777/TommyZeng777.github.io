3781dd3cb2942332b93c2aea0ac2d5d90d9d7f11
TommyZeng777 <tommyzeng1031@gmail.com>
2023-01-27 23:01:19 +0800

    补充更新

--- vuepress-theme-vdoing/components/Home.vue
+++ vuepress-theme-vdoing/components/Home.vue
@@ -468,7 +468,7 @@ export default {
     & + .main-wrapper
       margin-top: ($navbarHeight + 0.9rem)
   .main-wrapper
-    margin-top 2.2rem
+    margin-top 2.5rem
     .main-left
       .card-box
         margin-bottom 2rem
