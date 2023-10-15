8972eb9316b58f3e4f96926c52b943c781405aff
tommyzeng777 <tommyzeng1031@gmail.com>
2023-10-15 16:30:33 +0800

    重新更新

--- deploy.sh
+++ deploy.sh
@@ -3,61 +3,23 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+
+# push_addr=`git remote get-url --push origin` # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
+push_addr=git@github.com:TommyZeng777/TommyZeng777.github.io.git
+commit_info=`git describe --all --always --long`
+dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
+push_branch=gh-pages # 推送的分支
+
 # 生成静态文件
 npm run build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+cd $dist_path
 
-# deploy to github pages
-# echo 'tommyzeng.com' > CNAME
-
-# git init
-# git add .
-# git commit -m 'deploy'
-
-
-# # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git remote add origin git@github.com:TommyZeng777/TommyZeng777.github.io.git
-# git branch -M main
-# git push -f git@github.com:TommyZeng777/TommyZeng777.github.io.git main:gh-pages
-
-# cd -
-
-# if [ -z "$GITHUB_TOKEN" ]; then
-#   msg='deploy 脚本部署'
-#   githubUrl=git@github.com:TommyZeng777/TommyZeng777.github.io.git
-#   # giteeUrl=git@gitee.com:kele-bingtang/Kele-Bingtang.git
-# else
-#   msg='来自 Github actions 的自动部署'
-#   githubUrl=https://TommyZeng777:${GITHUB_TOKEN}@github.com/TommyZeng777/TommyZeng777.github.io.git
-#   git config --global user.name "TommyZeng777"
-#   git config --global user.email "tommyzeng1031@gmail.com"
-
-fi
 git init
 git add -A
-git commit -m "${msg}"
-git push -f $githubUrl master:gh-pages # 推送到 Github gh-pages 分支
+git commit -m "deploy, $commit_info"
+git push -f $push_addr HEAD:$push_branch
 
-
-# 退回开始所在目录
-cd - 
-# 删除打包项目
-rm -rf docs/.vuepress/dist
-
-
-# deploy to coding pages
-# echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
-# echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
-
-# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-#   codingUrl=git@e.coding.net:xgy/xgy.git
-# else
-#   codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/xgy/xgy.git
-# fi
-# git add -A
-# git commit -m "${msg}"
-# git push -f $codingUrl master # 推送到coding
-
-
+cd -
+rm -rf $dist_path
